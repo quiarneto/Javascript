82885cdfc9bea9ff248dd51ef883855aede8d991
quiarneto <quirino_neto@hotmail.com>
2022-07-21 01:04:09 -0300

    desafios das funções em JS

---
+++ desafio-alunos-aprovados/desafio-alunos-aprovados.js
@@ -0,0 +1,53 @@
+/** 
+ * Atividade 1: Alunos Aprovados
+ * Crie uma função que recebe o array alunos e um número que irá representar a média final;
+ * Percorra o array e popule um novo array auxiliar apenas com os alunos cujas notas são maiores ou iguais à média final;
+ * Utilize a técnica "object destructuring" para manipular as propriedades desejadas de cada aluno.
+*/
+
+const alunos = [
+	{
+		nome: 'João',
+		nota: 5,
+		turma: '1B',
+	},
+	{
+		nome: 'Sofia',
+		nota: 9,
+		turma: '1B',
+	},
+	{
+		nome: 'Paulo',
+		nota: 6,
+		turma: '2C',
+	},
+	{
+		nome: 'Miguel',
+		nota: 3,
+		turma: '2C',
+	},
+];
+
+function alunosAprovados(arr, media) {
+	let aprovados = [];
+
+	for (let i = 0; i < arr.length; i++) {
+
+		const {nota, nome} = arr[i]; //object destruction faz com que seja pré selecionado os itens do objeto, sem precisar ussar arr[i].algumaCoisa
+
+		if (nota >= media) {
+			aprovados.push(nome);
+		}
+	}
+
+	return aprovados;
+}
+
+console.log(alunosAprovados(alunos, 5));
+
+
+
+
+
+
+

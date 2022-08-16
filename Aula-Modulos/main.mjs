2845c8c414640adf76afd8b36a2b86a4807cad1d
quiarneto <quirino_neto@hotmail.com>
2022-08-15 23:11:22 -0300

    Adicinando OOJS, eventos, metodos, DOM

---
+++ Aula-Modulos/main.mjs
@@ -0,0 +1,5 @@
+import { mostraCidade, mostraIdade, mostraHobby } from "./funcoes.mjs";
+
+console.log(mostraIdade("Quirino", 31));
+console.log(mostraCidade("Quirino", "São Paulo"));
+console.log(mostraHobby("Quirino", "Jogar video-game"));
--- desafio-consumindo-APIs/assets/js/scripts.js
+++ desafio-consumindo-APIs/assets/js/scripts.js
@@ -1,11 +1,11 @@
-const BASE_URL = 'https://api.thecatapi.com/v1/images/search/';
+const BASE_URL = "https://api.thecatapi.com/v1/images/search/";
 
 const getCats = async () => {
 	try {
 		const data = await fetch(BASE_URL);
 		const json = await data.json();
         console.log(json.id);
-		return json.id;
+		return json[0].url;
 	} catch (e) {
 		console.log(e.message);
 	}
